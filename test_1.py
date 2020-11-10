9d3b2c1ccc9f527094762b7bcc07e997dfba7cdb
Teemmavong <Teemmavong@gmail.com>
2020-11-10 15:23:22 -0800

    finish pseudocode

--- test_1.py
+++ test_1.py
@@ -7,3 +7,26 @@ There will be 2 functions.
 and add that amount to the total.
 check if Option1 and Option2 are = or output the greater Option
 """
+"""
+# option1
+return 100x10
+
+#option2
+amount = 1
+list1 = []
+loop 10x
+add amount to list1
+amount *= 2
+sum = add all items in loop
+return sum
+
+# main
+if Option1 > Option2 
+reutrn Option1 is better
+
+if Options2 > Option1
+reutrn Option2 is better
+
+else 
+return they are equal
+"""
